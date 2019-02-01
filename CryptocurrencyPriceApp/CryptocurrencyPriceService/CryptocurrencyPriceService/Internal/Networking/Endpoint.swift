d520498a96e482f25bd147034c80cc0e50650090
Maxim Tsvetkov <777.maxxx@gmail.com>
2019-02-01 14:32:55 +0100

    Add endpoints for current price index

--- CryptocurrencyPriceApp/CryptocurrencyPriceService/CryptocurrencyPriceService/Internal/Networking/Endpoint.swift
+++ CryptocurrencyPriceApp/CryptocurrencyPriceService/CryptocurrencyPriceService/Internal/Networking/Endpoint.swift
@@ -10,15 +10,27 @@ import Foundation
 
 enum Endpoint {
     
+    case currentPriceIndex
+    case currentPriceIndexIn(Currency)
+    
     var method: Method {
         return .get
     }
     
     //as base url could be different for each endpoint, it makes sense to configure it separately (even probably in different place (not in Endpoint enum))
     //also url scheme could be configured separately
-    private var baseURL: String { return "" }
+    private var baseURL: String {
+        return "https://api.coindesk.com/v1/bpi"
+    }
     
-    private var path: String { return "" }
+    private var path: String {
+        switch self {
+        case .currentPriceIndex:
+            return "/currentprice.json"
+        case let .currentPriceIndexIn(currency):
+            return "/currentprice/\(currency).json"
+        }
+    }
     
     var url: URL? {
         
