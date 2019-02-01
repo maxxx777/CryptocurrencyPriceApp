e95f9bd5d35e6c7f0c0c4121ea0bc085ae9343d7
Maxim Tsvetkov <777.maxxx@gmail.com>
2019-02-01 14:39:27 +0100

    Add coindesk service with current price index endpoints

---
+++ CryptocurrencyPriceApp/CryptocurrencyPriceService/CryptocurrencyPriceService/Internal/Services/CoindeskService.swift
@@ -0,0 +1,76 @@
+//
+//  CoindeskService.swift
+//  CryptocurrencyPriceService
+//
+//  Created by MAXIM TSVETKOV on 01.02.19.
+//  Copyright © 2019 MAXIM TSVETKOV. All rights reserved.
+//
+
+import Foundation
+
+typealias Success<T> = ((T) -> Void)?
+typealias Failure = ((Error) -> Void)?
+
+protocol CoindeskService {
+    
+    // fetch current price index for specific currency
+    func fetchCurrentPriceIndex(in currency: Currency?, success: Success<PriceIndex>, failure: Failure)
+}
+
+extension CoindeskService {
+    
+    // fetch current price index for EUR, USD, GBP
+    func fetchCurrentPriceIndex(success: Success<PriceIndex>, failure: Failure) {
+        fetchCurrentPriceIndex(in: nil, success: success, failure: failure)
+    }
+}
+
+class CoindeskServiceImp {
+    
+    fileprivate var networking: Networking
+    
+    init(networking: Networking) {
+        self.networking = networking
+    }
+}
+
+extension CoindeskServiceImp: CoindeskService {
+    
+    func fetchCurrentPriceIndex(in currency: Currency?,
+                                success: ((PriceIndex) -> Void)?,
+                                failure: ((Error) -> Void)?) {
+        
+        let endpoint = Endpoint.makeCurrentPriceIndexEndpoint(with: currency)
+        
+        networking.call(endpoint) { result in
+            
+            switch result {
+            case let .success(data):
+                
+                do {
+                    let priceIndex = try JSONDecoder().decode(PriceIndex.self, from: data)
+                    success?(priceIndex)
+                } catch let jsonDecodingError {
+                    failure?(jsonDecodingError)
+                }
+
+            case let .failure(error):
+                
+                failure?(error)
+            }
+        }
+    }
+}
+
+fileprivate extension Endpoint {
+    
+    // factory to choose endpoint to get current price index depending on whether currency is specified or not
+    static func makeCurrentPriceIndexEndpoint(with currency: Currency?) -> Endpoint {
+        
+        if let currency = currency {
+            return Endpoint.currentPriceIndexIn(currency)
+        } else {
+            return Endpoint.currentPriceIndex
+        }
+    }
+}
