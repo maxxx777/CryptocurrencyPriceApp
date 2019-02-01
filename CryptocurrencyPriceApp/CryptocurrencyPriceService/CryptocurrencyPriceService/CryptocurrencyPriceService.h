c0680d2379b67c9d3d74394fc6cd0b9779eb049a
Maxim Tsvetkov <777.maxxx@gmail.com>
2019-02-01 10:00:45 +0100

    Create empty project for framework (price service)

---
+++ CryptocurrencyPriceApp/CryptocurrencyPriceService/CryptocurrencyPriceService/CryptocurrencyPriceService.h
@@ -0,0 +1,19 @@
+//
+//  CryptocurrencyPriceService.h
+//  CryptocurrencyPriceService
+//
+//  Created by MAXIM TSVETKOV on 01.02.19.
+//  Copyright © 2019 MAXIM TSVETKOV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CryptocurrencyPriceService.
+FOUNDATION_EXPORT double CryptocurrencyPriceServiceVersionNumber;
+
+//! Project version string for CryptocurrencyPriceService.
+FOUNDATION_EXPORT const unsigned char CryptocurrencyPriceServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptocurrencyPriceService/PublicHeader.h>
+
+
