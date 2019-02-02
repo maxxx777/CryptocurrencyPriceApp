d4759ff820eaeb98e179874be4664dc0030fc1f7
Maxim Tsvetkov <777.maxxx@gmail.com>
2019-02-02 19:34:26 +0100

    Add constants

---
+++ CryptocurrencyPriceApp/CryptocurrencyPriceApp/Constants/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  CryptocurrencyPriceApp
+//
+//  Created by MAXIM TSVETKOV on 02.02.19.
+//  Copyright © 2019 MAXIM TSVETKOV. All rights reserved.
+//
+
+import Foundation
+
+let DefaultErrorMessage = "Something went wrong. Please try again later."
