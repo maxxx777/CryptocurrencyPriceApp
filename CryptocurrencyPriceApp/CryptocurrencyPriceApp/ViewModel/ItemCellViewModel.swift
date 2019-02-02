174fc88fb188b27157d4605e4a1ad59e83727a66
Maxim Tsvetkov <777.maxxx@gmail.com>
2019-02-02 19:36:25 +0100

    Add item cell view model

---
+++ CryptocurrencyPriceApp/CryptocurrencyPriceApp/ViewModel/ItemCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCellViewModel.swift
+//  CryptocurrencyPriceApp
+//
+//  Created by MAXIM TSVETKOV on 02.02.19.
+//  Copyright © 2019 MAXIM TSVETKOV. All rights reserved.
+//
+
+import Foundation
+
+struct ItemCellViewModel {
+    
+    let date: String
+    let price: String
+}
