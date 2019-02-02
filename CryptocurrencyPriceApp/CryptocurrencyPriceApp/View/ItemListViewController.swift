d323c90c48c1f9db10355468e5fd3b7f33ff891e
Maxim Tsvetkov <777.maxxx@gmail.com>
2019-02-02 19:55:00 +0100

    Update item list view controller to display data, manage view states

--- CryptocurrencyPriceApp/CryptocurrencyPriceApp/View/ItemListViewController.swift
+++ CryptocurrencyPriceApp/CryptocurrencyPriceApp/View/ItemListViewController.swift
@@ -16,13 +16,85 @@ class ItemListViewController: UIViewController {
     
     //IBOutlets
     @IBOutlet weak var tableView: UITableView?
+    @IBOutlet weak var errorView: ErrorView?
+    @IBOutlet weak var loadingView: LoadingView?
     
     //Constants
     fileprivate let CellReuseIdentifier = "CellId"
     
+    //Properties
+    fileprivate var viewState: ViewState = .initiating {
+        didSet {
+            switch viewState {
+            case .data:
+                tableView?.isHidden = false
+                tableView?.reloadData()
+                loadingView?.isHidden = true
+                errorView?.isHidden = true
+            case .loading:
+                tableView?.isHidden = true
+                loadingView?.isHidden = false
+                errorView?.isHidden = true
+            case let .error(errorText):
+                tableView?.isHidden = true
+                loadingView?.isHidden = true
+                errorView?.isHidden = false
+                errorView?.labelText?.text = errorText
+            default: break
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureView()
+        fetchData()
+    }
+}
+
+extension ItemListViewController {
+    
+    func configureView() {
+        
+        navigationItem.title = "Bitcoin Price Index"
+        
+        viewState = .loading
+        
+        errorView?.buttonRetry?.addTarget(self,
+                                          action: #selector(retryFetchData),
+                                          for: .touchUpInside)
+    }
+    
+    func fetchData() {
+        
+        viewModel.fetchData(success: { [weak self] in
+            self?.viewState = .data
+            self?.listenCurrentUpdates()
+        }) { [weak self] errorText in
+            self?.viewState = .error(errorText)
+        }
+    }
+    
+    func listenCurrentUpdates() {
+        
+        viewModel.listenCurrentUpdates(success: { [weak self] in
+            self?.reloadFirstRow()
+        }) { [weak self] errorText in
+            self?.viewState = .error(errorText)
+        }
+    }
+    
+    func reloadFirstRow() {
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView?.reloadRows(at: [indexPath], with: .fade)
+    }
+    
+    @objc func retryFetchData() {
+        
+        viewState = .loading
+        fetchData()
     }
 }
 
@@ -37,11 +109,15 @@ extension ItemListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return viewModel.numberOfItems()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: CellReuseIdentifier, for: indexPath) 
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellReuseIdentifier, for: indexPath)  as! ItemTableViewCell
+        
+        let cellViewModel = viewModel.cellViewModel(at: indexPath.row)
+        
+        cell.viewModel = cellViewModel
         
         return cell
     }
