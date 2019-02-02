b4fe6874128c35e19c79b6fbd56511e8d5ffb875
Maxim Tsvetkov <777.maxxx@gmail.com>
2019-02-03 00:53:01 +0100

    Update today view controller

--- CryptocurrencyPriceApp/TodayWidget/TodayViewController.swift
+++ CryptocurrencyPriceApp/TodayWidget/TodayViewController.swift
@@ -9,21 +9,34 @@
 import UIKit
 import NotificationCenter
 
-class TodayViewController: UIViewController, NCWidgetProviding {
-        
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
-    }
-        
-    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-        
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-        
-        completionHandler(NCUpdateResult.newData)
-    }
+class TodayViewController: UIViewController {
     
+    //dependencies
+    let viewModel = TodayViewModelImp()
+    
+    //IB outlets
+    @IBOutlet weak var labelPrice: UILabel?
+}
+
+extension TodayViewController: NCWidgetProviding {
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        
+        viewModel.listenCurrentUpdates(success: { [weak self] priceText in
+            
+            self?.updateView(with: priceText)
+            completionHandler(NCUpdateResult.newData)
+        }) { [weak self] errorText in
+            self?.updateView(with: errorText)
+            completionHandler(NCUpdateResult.failed)
+        }
+    }
+}
+
+extension TodayViewController {
+
+    func updateView(with priceText: String) {
+        
+        labelPrice?.text = priceText
+    }
 }
